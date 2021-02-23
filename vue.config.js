ba8707ad8939dc0a8756ecec93258837d6200be1
arsiac <arsia.chou@gmail.com>
2021-02-23 12:15:55 +0800

    修改webpack配置

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 'use strict'
+// const path = require('path')
+//
+// function resolve (dir) {
+//   return path.join(__dirname, '..', dir)
+// }
 
 module.exports = {
   publicPath: '/',
   outputDir: 'dist',
-  assetsDir: 'static',
   lintOnSave: true,
   productionSourceMap: false,
   runtimeCompiler: false,
@@ -11,17 +15,16 @@ module.exports = {
     port: 9000,
     open: true,
     overlay: {
-      warnings: false,
+      warnings: true,
       errors: true
     }
-    // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
     name: 'Psychology',
     resolve: {
-      alias: {}
+      alias: {
+        // '@':resolve('src')
+      }
     }
   }
 }
