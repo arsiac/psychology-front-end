48e743f05b016e08efaf8b4597a1a52a4b32c5e1
arsiac <arsia.chou@gmail.com>
2021-02-23 12:17:33 +0800

    去除多余import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import ElementUI, { Message, MessageBox } from 'element-ui'
+import ElementUI from 'element-ui'
 
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
@@ -12,9 +12,10 @@ Vue.use(less)
 Vue.use(ElementUI)
 
 // 挂载全局
+
 Vue.prototype.$auth = isAuth
-Vue.prototype.$message = Message
-Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$message = ElementUI.Message
+Vue.prototype.$confirm = ElementUI.MessageBox.confirm
 
 Vue.config.productionTip = false
 
