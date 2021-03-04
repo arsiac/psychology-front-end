16a97b5caec2fd06447ad9b7d7178479efe7e025
arsiac <arsia.chou@gmail.com>
2021-03-04 17:37:35 +0800

    资源CURD

--- src/views/centre/resource/index.vue
+++ src/views/centre/resource/index.vue
@@ -1,9 +1,224 @@
 <template>
-  <span>Resource</span>
+  <div class="mod-config">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.name" placeholder="名称" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="dataForm.type" placeholder="类型">
+          <el-option label="无" :value="null"/>
+          <el-option label="分组" :value="1"/>
+          <el-option label="菜单" :value="2"/>
+          <el-option label="权限" :value="3"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+        <el-button v-if="$auth('centre/resource', 'post')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button
+            v-if="$auth('centre/resource', 'delete')"
+            type="danger" @click="deleteHandle()"
+            :disabled="dataListSelections.length <= 0">批量删除
+        </el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table
+        :data="dataList"
+        border
+        v-loading="dataListLoading"
+        @selection-change="selectionChangeHandle"
+        max-height="500"
+        style="width: 100%;">
+      <el-table-column
+          type="selection"
+          header-align="center"
+          align="center"
+          width="50"
+          fixed="left"
+      >
+      </el-table-column>
+      <el-table-column
+          header-align="center"
+          align="center"
+          label="序号"
+          width="80"
+          fixed="left"
+      >
+        <template slot-scope="scope">
+          <span>{{ scope.$index + 1 }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          header-align="center"
+          align="center"
+          label="资源名称">
+      </el-table-column>
+      <el-table-column
+          prop="icon"
+          header-align="center"
+          align="center"
+          label="图标"
+          width="80"
+      >
+        <template slot-scope="scope">
+          <i v-if="scope.row.icon && scope.row.icon !== ''" :class="'fa ' + scope.row.icon"></i>
+          <span v-else>-</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          prop="uri"
+          header-align="center"
+          align="center"
+          label="URI">
+        <template slot-scope="scope">
+          <span v-if="scope.row.uri">{{ scope.row.uri }}</span>
+          <span v-else>-</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          prop="type"
+          header-align="center"
+          align="center"
+          label="类型"
+          width="100"
+      >
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.type === 1" type="warning">分组</el-tag>
+          <el-tag v-else-if="scope.row.type === 2" type="success">菜单</el-tag>
+          <el-tag v-else-if="scope.row.type === 3" type="info">权限</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column
+          prop="parent"
+          header-align="center"
+          align="center"
+          label="所属">
+        <template slot-scope="scope">
+          <span v-if="scope.row.parent">{{ scope.row.parent }}</span>
+          <span v-else>-</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          fixed="right"
+          header-align="center"
+          align="center"
+          width="150"
+          label="操作"
+      >
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row)">修改</el-button>
+          <el-button type="text" size="small" @click="deleteHandle(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+        @size-change="sizeChangeHandle"
+        @current-change="currentChangeHandle"
+        :current-page="pageIndex"
+        :page-sizes="[10, 20, 50, 100]"
+        :page-size="pageSize"
+        :total="totalPage"
+        layout="total, sizes, prev, pager, next, jumper"
+        style="margin-top: 20px;"
+    >
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+  </div>
 </template>
 
 <script>
-export default {}
+import resourceApi from '@/api/centre/resource'
+import AddOrUpdate from './resource-add-or-update'
+
+export default {
+  data () {
+    return {
+      dataForm: {
+        name: null,
+        type: null
+      },
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false,
+      dataListSelections: [],
+      addOrUpdateVisible: false
+    }
+  },
+  components: {
+    AddOrUpdate
+  },
+  mounted () {
+    this.getDataList()
+  },
+  methods: {
+    // 获取数据列表
+    getDataList () {
+      this.dataListLoading = true
+      resourceApi.fuzzy(Object.assign({}, this.dataForm, {
+        pageNum: this.pageIndex,
+        pageSize: this.pageSize
+      })).then(({ data }) => {
+        this.dataList        = data.data
+        this.totalPage       = parseInt(data.total)
+        this.dataListLoading = false
+      }).catch(_ => {
+        this.dataListLoading = false
+      })
+    },
+    // 每页数
+    sizeChangeHandle (val) {
+      this.pageSize  = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+    // 当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    },
+    // 多选
+    selectionChangeHandle (val) {
+      this.dataListSelections = val
+    },
+    // 新增 / 修改
+    addOrUpdateHandle (id) {
+      this.addOrUpdateVisible = true
+      this.$nextTick(() => {
+        this.$refs.addOrUpdate.init(id)
+      })
+    },
+    // 删除
+    deleteHandle (row) {
+      const rows = row ? [row] : this.dataListSelections
+      this.$confirm(`确定对[${rows.map(item => item.name).join(',')}]进行[${row ? '删除' : '批量删除'}]操作?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        resourceApi.batchRemove(rows).then(({ data }) => {
+          if (data.data) {
+            this.$notify.success({
+              message: '删除成功',
+              title: '删除'
+            })
+          }
+        })
+      }).catch(_ => {
+        this.$notify.info({
+          message: '用户取消操作',
+          title: '删除'
+        })
+      })
+    }
+  }
+}
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+@import "public/theme.less";
+</style>
---
+++ src/views/centre/resource/resource-add-or-update.vue
@@ -0,0 +1,132 @@
+<template>
+  <el-dialog
+      :title="title"
+      :close-on-click-modal="false"
+      :visible.sync="visible">
+    <el-form
+        :model="dataForm"
+        :rules="dataRule"
+        ref="dataForm"
+        @keyup.enter.native="dataFormSubmit()"
+        label-width="80px"
+    >
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item label="资源名称" prop="name">
+            <el-input v-model="dataForm.name" placeholder="资源名称"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="类型" prop="type">
+            <el-select v-model="dataForm.type">
+              <el-option label="分组" :value="1"/>
+              <el-option label="菜单" :value="2"/>
+              <el-option label="权限" :value="3"/>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item v-if="dataForm.type !== 1" label="父节点" prop="parent">
+            <el-autocomplete
+                v-model="searchData"
+                :fetch-suggestions="queryResource"
+                placeholder="请输入内容"
+                value-key="name"
+                @select="handleResourceSelect"
+            ></el-autocomplete>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item v-if="dataForm.type === 2" label="URI" prop="uri">
+            <el-input v-model="dataForm.uri" placeholder="URI"></el-input>
+          </el-form-item>
+          <el-form-item v-if="dataForm.type === 3" label="权限" prop="uri">
+            <el-select v-model="dataForm.uri">
+              <el-option label="查看" value="op:get"/>
+              <el-option label="添加" value="op:post"/>
+              <el-option label="修改" value="op:put"/>
+              <el-option label="删除" value="op:delete"/>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="24">
+          <el-form-item v-if="dataForm.type === 2" label="图标" prop="icon">
+            <el-input v-model="dataForm.icon" placeholder="图标"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import resourceApi from '@/api/centre/resource'
+
+export default {
+  data () {
+    return {
+      visible: false,
+      title: '',
+      dataForm: { id: 0 },
+      searchData: '',
+      dataRule: {
+        name: [
+          { required: true, message: '资源名称不能为空', trigger: 'blur' }
+        ],
+        type: [
+          { required: true, message: '类型(1分组,2菜单,3按钮)不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    init (row) {
+      this.dataForm = Object.assign({}, row) || {}
+      this.title    = !row ? '新增' : '修改'
+      this.visible  = true
+    },
+    queryResource (name, cb) {
+      resourceApi.fuzzy({ name }).then(({ data }) => {
+        let list = []
+        if (Array.isArray(data.data)) {
+          list = data.data
+        }
+        if (list.length === 0) {
+          // 查询为空,清空选择
+          this.dataForm.parent = null
+        }
+        cb(list)
+      })
+    },
+    handleResourceSelect (value) {
+      this.dataForm.parent = value.id
+    },
+    // 表单提交
+    dataFormSubmit () {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const submit = this.dataForm.id ? resourceApi.modify : resourceApi.add
+          submit(this.dataForm).then(({ data }) => {
+            if (data.data) {
+              this.$notify.success({
+                title: this.title,
+                message: '操作成功'
+              })
+              this.visible = false
+              this.$emit('refreshDataList')
+            }
+          })
+        }
+      })
+    }
+  }
+}
+</script>
