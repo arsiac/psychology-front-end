ca55401225d3d08f1827b27caf054b6ce8720254
arsiac <arsia.chou@gmail.com>
2021-03-06 17:53:36 +0800

    首页

--- src/views/common/home/index.vue
+++ src/views/common/home/index.vue
@@ -1,8 +1,35 @@
 <template>
-    <div class="home">
-      HOME
+  <div class="home">
+    <div class="title">
+      <div class="welcome">欢迎使用</div>
+      <div class="app-name">心理学院信息管理系统</div>
     </div>
+  </div>
 </template>
 
-<script src="./_home.js" lang="js"></script>
-<style src="./_home.less" lang="less" scoped></style>
+<script lang="js">
+export default {}
+</script>
+<style lang="less" scoped>
+.home {
+  width: 100%;
+
+  .title {
+    padding-left: 30px;
+    padding-top: 30px;
+    position: absolute;
+
+    .welcome {
+      font-size: 4em;
+      font-weight: bold;
+    }
+
+    .app-name {
+      margin-top: 10px;
+      font-size: 2.8em;
+      font-weight: normal;
+    }
+  }
+
+}
+</style>
