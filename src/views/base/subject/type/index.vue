a8044f86b3db66263f94625eaf39dadfe87a3116
arsiac <arsia.chou@gmail.com>
2021-03-06 20:52:03 +0800

    完成课题类型管理

---
+++ src/views/base/subject/type/index.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="mod-config">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.name" placeholder="name" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+        <el-button v-if="$auth(authUrl, 'post')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button v-if="$auth(authUrl, 'delete')" type="danger" @click="deleteHandle()"
+                   :disabled="dataListSelections.length <= 0">批量删除
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+        :data="dataList"
+        border
+        v-loading="dataListLoading"
+        @selection-change="selectionChangeHandle"
+        max-height="500"
+        style="width: 100%;">
+      <el-table-column
+          type="selection"
+          header-align="center"
+          align="center"
+          width="50">
+      </el-table-column>
+      <el-table-column
+          header-align="center"
+          align="center"
+          label="序号"
+          width="80"
+          fixed="left"
+      >
+        <template slot-scope="scope">
+          <span>{{ scope.$index + 1 }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          header-align="center"
+          align="center"
+          label="课题类别">
+      </el-table-column>
+      <el-table-column
+          fixed="right"
+          header-align="center"
+          align="center"
+          width="150"
+          label="操作">
+        <template slot-scope="scope">
+          <el-button
+              type="text"
+              size="small"
+              @click="addOrUpdateHandle(scope.row)"
+              :disabled="!$auth(authUrl, 'put')"
+          >修改
+          </el-button>
+          <el-button
+              type="text"
+              size="small"
+              @click="deleteHandle(scope.row)"
+              :disabled="!$auth(authUrl, 'delete')"
+          >删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+        @size-change="sizeChangeHandle"
+        @current-change="currentChangeHandle"
+        :current-page="pageIndex"
+        :page-sizes="[10, 20, 50, 100]"
+        :page-size="pageSize"
+        :total="totalPage"
+        style="margin-top: 20px;"
+        layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+  </div>
+</template>
+
+<script>
+import subjectTypeApi from '@/api/base/subject-type'
+import AddOrUpdate    from './subject-type-add-or-update'
+
+export default {
+  data () {
+    return {
+      authUrl: 'base/subject/type',
+      dataForm: {
+        name: ''
+      },
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false,
+      dataListSelections: [],
+      addOrUpdateVisible: false
+    }
+  },
+  components: {
+    AddOrUpdate
+  },
+  activated () {
+    this.getDataList()
+  },
+  methods: {
+    // 获取数据列表
+    getDataList () {
+      this.dataListLoading = true
+      subjectTypeApi.fuzzy(Object.assign({}, this.dataForm, {
+        pageNum: this.pageIndex,
+        pageSize: this.pageSize
+      })).then(({ data }) => {
+        this.dataList        = data.data
+        this.totalPage       = parseInt(data.total)
+        this.dataListLoading = false
+      }).catch(_ => {
+        this.dataListLoading = false
+      })
+    },
+    // 每页数
+    sizeChangeHandle (val) {
+      this.pageSize  = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+    // 当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    },
+    // 多选
+    selectionChangeHandle (val) {
+      this.dataListSelections = val
+    },
+    // 新增 / 修改
+    addOrUpdateHandle (id) {
+      this.addOrUpdateVisible = true
+      this.$nextTick(() => {
+        this.$refs.addOrUpdate.init(id)
+      })
+    },
+    // 删除
+    deleteHandle (row) {
+      const rows = row ? [row] : this.dataListSelections
+      this.$confirm(`确定对[${rows.map(item => item.name).join(',')}]进行[${row ? '删除' : '批量删除'}]操作?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        subjectTypeApi.remove(rows).then(({ data }) => {
+          if (data.data) {
+            this.$notify.success({
+              message: '删除成功',
+              title: '删除'
+            })
+            this.getDataList()
+          }
+        })
+      }).catch(_ => {
+        this.$notify.info({
+          message: '用户取消操作',
+          title: '删除'
+        })
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/base/subject/type/subject-type-add-or-update.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-dialog
+      :title="title"
+      width="400px"
+      :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()"
+             label-width="80px">
+      <el-form-item label="课题类别" prop="name">
+        <el-input v-model="dataForm.name" placeholder="课题类别"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import subjectTypeApi from '@/api/base/subject-type'
+
+export default {
+  data () {
+    return {
+      title: '',
+      visible: false,
+      dataForm: {},
+      dataRule: {
+        name: [
+          { required: true, message: '课题类别不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    init (row) {
+      this.dataForm = Object.assign({}, row) || {}
+      this.title    = !row ? '新增' : '修改'
+      this.visible  = true
+    },
+    // 表单提交
+    dataFormSubmit () {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const submit = this.dataForm.id ? subjectTypeApi.modify : subjectTypeApi.add
+          submit(this.dataForm).then(({ data }) => {
+            if (data.data) {
+              this.$notify.success({
+                title: this.title,
+                message: '操作成功'
+              })
+              this.visible = false
+              this.$emit('refreshDataList')
+            }
+          })
+        }
+      })
+    }
+  }
+}
+</script>
