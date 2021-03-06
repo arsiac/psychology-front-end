9b8624169cf6d66702d9232e36f4552701e3a432
arsiac <arsia.chou@gmail.com>
2021-03-06 20:51:21 +0800

    修改格式

--- src/views/base/department/index.vue
+++ src/views/base/department/index.vue
@@ -51,8 +51,20 @@
           width="150"
           label="操作">
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row)" :disabled="!$auth(authUrl, 'put')">修改</el-button>
-          <el-button type="text" size="small" @click="deleteHandle(scope.row)" :disabled="!$auth(authUrl, 'delete')">删除</el-button>
+          <el-button
+              type="text"
+              size="small"
+              @click="addOrUpdateHandle(scope.row)"
+              :disabled="!$auth(authUrl, 'put')"
+          >修改
+          </el-button>
+          <el-button
+              type="text"
+              size="small"
+              @click="deleteHandle(scope.row)"
+              :disabled="!$auth(authUrl, 'delete')"
+          >删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
